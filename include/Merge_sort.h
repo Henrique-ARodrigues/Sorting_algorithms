830ecf65ecde26ec4eff77ad78409dfbe29dca3f
Henrique <henriquealvesrodrigues124@gmail.com>
2025-10-04 18:12:45 -0300

    Include file

--- include/Merge_sort.h
+++ include/Merge_sort.h
@@ -1,6 +1,7 @@
 #ifndef MERGE_SORT_H
 #define MERGE_SORT_H
 
-void Merge_sort();
+void Merge_sort(int arr[], int inicio, int fim, int* comp, int* swap);
+
 
 #endif
---
+++ include/Quick_sort.h
@@ -0,0 +1,6 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H    
+
+void Quick_sort(int arr[], int tamanho, int *comp, int *swap);
+
+#endif
